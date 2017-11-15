790370c81c465c262b5b5d6603d9604f3409849e
xxy130330 <yangxue0708@gmail.com>
2017-11-15 14:38:42 -0800

    fix merge conflict

--- misc/roadmap.js
+++ misc/roadmap.js
@@ -1,17 +1,14 @@
 $(document).ready(function(){
-<<<<<<< HEAD
     //we can either put click handlers here or link to an initialize function
     //for the board, I think we should use delegated click handlers based in a container obj
     game = new Game();
     // $('.container').on('click','div.square', checkIfMoveIsLegal);
     buildBoard();
-=======
   //we can either put click handlers here or link to an initialize function
   //for the board, I think we should use delegated click handlers based in a container obj
-  game = new Game();
-  $('.container').on('click','div.square', checkIfMoveIsLegal);
-  buildBoard();
->>>>>>> 6c10539cb40d3b247c1403bb8989572039e5b34a
+  //   game = new Game();
+    $('.container').on('click','div.square', checkIfMoveIsLegal);
+    buildBoard();
 });
 
 var game;
@@ -62,7 +59,6 @@ function resetGame(){
   game = new Game()
 }
 
-<<<<<<< HEAD
 function buildBoard() {
     $('.container').empty();
     var createRow = $('<div>').addClass('row');
@@ -86,48 +82,21 @@ function buildBoard() {
             }
         }
     }
-  // if($('div.square').attr('row','3').attr('col','3')) {
-  //     $('div.square').append(blackPiece);
-  // }
 
 
-  //this is the dom creation of the board, maybe make part of Game object
-=======
-function buildBoard(){
-  $('.container').empty();
-  var createRow= $('<div>').addClass('row');
-  for(var i=0; i<8;i++){
-    $('.container').append(createRow);
-    for(var j=0; j<8; j++){
-      var blackPiece= $('<div>').addClass('black');
-      var whitePiece= $('<div>').addClass('white');
-      var createColumn= $('<div>',{
-        class:'square',
-        attr: {
-          row: i,
-          col: j}
-        });
-      $('.row:last-child').append(createColumn);
-      if(game.gameboard[i][j]==='b'){
-        $('.row:last-child .square:last-child').append(blackPiece);
-      }else if(game.gameboard[i][j]==='w'){
-        $('.row:last-child .square:last-child').append(whitePiece);
-      }
+    //this is the dom creation of the board, maybe make part of Game object
+
+}
+
+
+    function updateDisplay() {
+        //Update the board position and points in the display
     }
-  }
->>>>>>> 6c10539cb40d3b247c1403bb8989572039e5b34a
-}
 
-
-
-function updateDisplay(){
-  //Update the board position and points in the display
-}
-
-function checkWinState(){
-  //determine if a win state has been reached
-  //this can maybe be accomplished purely based on turn number
-}
+    function checkWinState() {
+        //determine if a win state has been reached
+        //this can maybe be accomplished purely based on turn number
+    }
 
 // function checkIfMoveIsLegal(arr) {
 //     //console.log("Checking if Move is Legal")
@@ -143,75 +112,73 @@ function checkWinState(){
 //             }
 // }
 
-function getOpponentName(){
-  return game.currentPlayer === 'b' ? 'w' : 'b';
-}
-
-function handleMove(startingPosArr){
-  var piecesFlipped = null;
-  var directions = {//col then row
-    'w': [0,-1],
-    'nw': [-1,-1],
-    'n': [-1,0],
-    'ne': [-1,1],
-    'e': [0,1],
-    'se': [1,1],
-    's': [1,0],
-    'sw': [1,-1]
-  }
-
-  var validDirections = [];
-  var moveCount = 0;
-
-  if (game.gameboard[startingPosArr[0]][startingPosArr[1]]){
-    for (item in directions){
-      checkDirection(item,startingPosArr);
-      moveCount = 0;
+    function getOpponentName() {
+        return game.currentPlayer === 'b' ? 'w' : 'b';
     }
 
-    if (validDirections.length > 0){
-      //add player's piece to board here
-      for (var i = 0; i < validDirections.length; i++){
-        flipPieces(directions.validDirections[i],startingPosArr);
-      }
-      return true
-    } else {
-      return false;
+    function handleMove(startingPosArr) {
+        var piecesFlipped = null;
+        var directions = {//col then row
+            'w': [0, -1],
+            'nw': [-1, -1],
+            'n': [-1, 0],
+            'ne': [-1, 1],
+            'e': [0, 1],
+            'se': [1, 1],
+            's': [1, 0],
+            'sw': [1, -1]
+        };
+
+        var validDirections = [];
+        var moveCount = 0;
+
+        if (game.gameboard[startingPosArr[0]][startingPosArr[1]]) {
+            for (item in directions) {
+                checkDirection(item, startingPosArr);
+                moveCount = 0;
+            }
+
+            if (validDirections.length > 0) {
+                //add player's piece to board here
+                for (var i = 0; i < validDirections.length; i++) {
+                    flipPieces(directions.validDirections[i], startingPosArr);
+                }
+                return true
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+
+
+        function checkDirection(direction, startPoint) {//direction is a string (a valid key the obj)
+            var currentPos = startPoint.slice();
+            var newPos = [currentPos[0] + directions.direction[0], currentPos[1] + directions.direction[1]];
+            if (game.gameboard[newPos[0]][newPos[1]] === game.getOpponentName()) {
+                moveCount++;
+                checkDirection(direction, newPos);
+            } else if (game.gameboard[newPos[0]][newPos[1]] === game.currentPlayer && moveCount > 0) {
+                validDirections.push(direction);
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        function flipPieces(direction, startPoint) {//direction is a string (from validDirections)
+            var currentPos = startingPosArr.slice();
+            var newPos = [currentPos[0] + directions.direction[0], currentPos[1] + directions.direction[1]];
+            if (game.gameboard[newPos[0]][newPos[1]] === game.getOpponentName()) {
+                game.gameboard[newPos[0]][newPos[1]] = game.currentPlayer;
+                piecesFlipped++;
+                flipPieces(direction, newPos);
+            }
+
+            //does a tree search for all possible pieces affected by move
+            //update the game objects gameboard
+            game.updateScore(piecesFlipped);
+            updateDisplay();
+        }
     }
-  } else {
-    return false;
-  }
 
-
-
-
-  function checkDirection(direction,startPoint){//direction is a string (a valid key the obj)
-    var currentPos = startPoint.slice();
-    var newPos = [currentPos[0]+directions.direction[0],currentPos[1]+directions.direction[1]];
-    if (game.gameboard[newPos[0]][newPos[1]] === game.getOpponentName()){
-      moveCount++;
-      checkDirection(direction,newPos);
-    } else if (game.gameboard[newPos[0]][newPos[1]] === game.currentPlayer && moveCount > 0){
-      validDirections.push(direction);
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  function flipPieces(direction,startPoint){//direction is a string (from validDirections)
-    var currentPos = startingPosArr.slice();
-    var newPos = [currentPos[0]+directions.direction[0],currentPos[1]+directions.direction[1]];
-    if (game.gameboard[newPos[0]][newPos[1]] === game.getOpponentName()){
-      game.gameboard[newPos[0]][newPos[1]] = game.currentPlayer;
-      piecesFlipped++;
-      flipPieces(direction,newPos);
-    }
-
-    //does a tree search for all possible pieces affected by move
-    //update the game objects gameboard
-    game.updateScore(piecesFlipped);
-    updateDisplay();
-  }
-}
->>>>>>> 6c10539cb40d3b247c1403bb8989572039e5b34a
